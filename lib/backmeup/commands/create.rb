f8c67cf787d2756acb607384e30e9b717e292046
thomasregnet <ossi@asgard.localdomain>
2020-09-07 09:17:41 +0200

    Write pseudo code for Backmeup::Commands::Create
    Just to get an outline.

--- lib/backmeup/commands/create.rb
+++ lib/backmeup/commands/create.rb
@@ -4,15 +4,34 @@ require_relative '../command'
 
 module Backmeup
   module Commands
+    # Create a backup
     class Create < Backmeup::Command
       def initialize(repository, options)
         @repository = repository
         @options = options
       end
 
+      attr_reader :options, :repository
+
       def execute(input: $stdin, output: $stdout)
-        # Command logic goes here ...
-        output.puts "OK"
+        # previous_destination = Expire.newest
+        # destination = DateTime.now.to_s
+        # CreateDestinationAction.perform(
+        #   destination: destination,
+        #   previous_destination: previous_destination,
+        #   root: root
+        # )
+
+        # CreateBackupAction.perform(
+        #   destination: destination,
+        #   previous_destination: previous_destination,
+        #   root: root
+        # )
+        output.puts('OK')
+      end
+
+      def root
+        @root ||= Backmeup::Root.new(repository)
       end
     end
   end
