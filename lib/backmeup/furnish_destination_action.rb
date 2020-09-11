2685fa93ddb1acc3859eb9e78e1b10011a32fbae
thomasregnet <ossi@asgard.localdomain>
2020-09-11 12:00:48 +0200

    Remove fragments from FurnishDestinationAction

--- lib/backmeup/furnish_destination_action.rb
+++ lib/backmeup/furnish_destination_action.rb
@@ -7,12 +7,6 @@ module Backmeup
       new(**args).perform
     end
 
-    # def initialize(destination_path:)
-    #   @destination_path = destination_path
-    # end
-
-    # attr_reader :destination_path
-
     def initialize(layout:)
       @layout = layout
     end
@@ -20,23 +14,9 @@ module Backmeup
     attr_reader :layout
 
     def perform
-      # create_destination_data
-      # create_destination_log('stdout')
-      # create_destination_log('stderr')
       FileUtils.mkpath(layout.data)
       File.open(layout.stderr, 'w') { |file| file.truncate(0) }
       File.open(layout.stdout, 'w') { |file| file.truncate(0) }
-    end
-
-    private
-
-    def create_destination_data
-      FileUtils.mkpath(File.join(destination_path, 'data'))
-    end
-
-    def create_destination_log(file_name)
-      log_file = File.join(destination_path, file_name)
-      File.open(log_file, 'w') { |file| file.truncate(0) }
     end
   end
 end
