8e1e911e6235d6aa18d0e52569d69c668404f4f8
thomasregnet <ossi@asgard.localdomain>
2020-09-14 11:58:15 +0200

    Add specs for ScriptableAction#perform

--- spec/support/shared_examples_for_scriptable_actions.rb
+++ spec/support/shared_examples_for_scriptable_actions.rb
@@ -1,7 +1,31 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'a scriptable action' do
-  describe 'methods the mixin defines' do
-    it { should respond_to(:perform) }
+  it { should respond_to(:perform) }
+
+  describe '#perform' do
+    context 'when a script exists' do
+      before do
+        allow(subject).to receive('script_exists?').and_return(true)
+        allow(subject).to receive(:perform_with_script)
+      end
+
+      it 'calls #perform_with_script' do
+        subject.perform
+        expect(subject).to have_received(:perform_with_script)
+      end
+    end
+
+    context 'when no script exists' do
+      before do
+        allow(subject).to receive('script_exists?').and_return(false)
+        allow(subject).to receive(:perform_without_script)
+      end
+
+      it 'calls #perform_without_script' do
+        subject.perform
+        expect(subject).to have_received(:perform_without_script)
+      end
+    end
   end
 end
