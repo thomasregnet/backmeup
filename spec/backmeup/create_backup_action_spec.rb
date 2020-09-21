3bf498471098083c76ec39cf2364aaeae475830c
thomasregnet <ossi@asgard.localdomain>
2020-09-21 13:47:07 +0200

    Improve create_backup_action_spec.rb

--- spec/backmeup/create_backup_action_spec.rb
+++ spec/backmeup/create_backup_action_spec.rb
@@ -26,15 +26,15 @@ RSpec.describe Backmeup::CreateBackupAction do
   describe '.perform' do
     let(:repository) { File.join('tmp', 'repository') }
 
-    let(:backups) { File.join(repository, 'backups') }
-    let(:config_dir) { File.join(repository, 'config') }
-    let(:destination) { '2020-01-01T11:24:00+02:00' }
+    let(:backups) { File.join('tmp', 'repository', 'backups') }
+    let(:destination) { File.join('tmp', 'repository', 'backups', '2020-01-01T11:24:00+02:00') }
     let(:source) { File.join('tmp', 'source') }
 
     before do
       FileUtils.mkpath(File.join(source))
       FileUtils.touch(File.join(source, 'data.txt'))
 
+      config_dir = File.join('tmp', 'repository', 'config')
       FileUtils.mkpath(config_dir)
       File.open(File.join(config_dir, 'filelist'), 'w') do |file|
         file.puts(File.join('tmp', 'source'))
@@ -49,43 +49,47 @@ RSpec.describe Backmeup::CreateBackupAction do
     end
 
     context 'without a script' do
-      it 'creates a backup' do
-        expected_file = File.join(
+      let(:expected_file) do
+        File.join(
           repository, 'backups', destination, 'data', 'source', 'data.txt'
         )
+      end
 
+      before do
         described_class.perform(
           destination:          destination,
           previous_destination: nil,
           root:                 Backmeup::Root.new(repository)
         )
+      end
+
+      it 'creates a backup' do
         expect(Pathname.new(expected_file)).to exist
       end
     end
 
     context 'with a create_backup script' do
-      let(:creator) do
-        described_class.new(
+      let(:cmd) { instance_double('TTY::Command') }
+
+      before do
+        bin_dir = File.join(repository, 'bin')
+        FileUtils.mkpath(bin_dir)
+        FileUtils.touch(File.join(bin_dir, 'create_backup'))
+        FileUtils.chmod(0o755, File.join(bin_dir, 'create_backup'))
+
+        creator = described_class.new(
           destination:          destination,
           previous_destination: nil,
           root:                 Backmeup::Root.new(repository)
         )
-      end
-
-      let(:bin) { File.join(repository, 'bin') }
-      let(:cmd) { instance_double('TTY::Command') }
-
-      before do
-        FileUtils.mkpath(bin)
-        FileUtils.touch(File.join(bin, 'create_backup'))
-        FileUtils.chmod(0o755, File.join(bin, 'create_backup'))
 
         allow(creator).to receive(:cmd).and_return(cmd)
         allow(cmd).to receive(:run)
+
+        creator.perform
       end
 
       it 'calls that script' do
-        creator.perform
         expect(cmd).to have_received(:run)
       end
     end
