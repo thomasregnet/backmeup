7f40593afb5bf0ed29ccbad84fdbdeeea754d78b
thomasregnet <ossi@asgard.localdomain>
2020-09-21 14:32:11 +0200

    Improve spec/integration/init_spec.rb

--- spec/integration/init_spec.rb
+++ spec/integration/init_spec.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe '`backmeup init` command', type: :cli do
-  it 'executes `backmeup help init` command successfully' do
-    output = `backmeup help init`
-    expected_output = <<~OUT
+  let(:expected_output) do
+    <<~OUT
       Usage:
         backmeup init PATH
-      
+
       Options:
         -h, [--help], [--no-help]  # Display usage information
-      
+
       Command description...
     OUT
+  end
 
+  it 'executes `backmeup help init` command successfully' do
+    output = `backmeup help init`
     expect(output).to eq(expected_output)
   end
 end
