fdd421b8c26f4e095a9a008d91af987c4e9a2bbb
thomasregnet <ossi@asgard.localdomain>
2020-09-21 13:52:53 +0200

    Improve spec/integration/create_spec.rb

--- spec/integration/create_spec.rb
+++ spec/integration/create_spec.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe '`backmeup create` command', type: :cli do
-  it 'executes `backmeup help create` command successfully' do
-    output = `backmeup help create`
-    expected_output = <<~OUT
+  let(:expected_output) do
+    <<~OUT
       Usage:
         backmeup create REPOSITORY
-      
+
       Options:
         -h, [--help], [--no-help]  # Display usage information
-      
+
       Command description...
     OUT
+  end
 
+  it 'executes `backmeup help create` command successfully' do
+    output = `backmeup help create`
     expect(output).to eq(expected_output)
   end
 end
