83f5fe2754861b09926f377940136dd2a1646218
thomasregnet <ossi@asgard.localdomain>
2020-09-22 09:07:40 +0200

    Remove spec-loop for hookable actions

--- spec/support/shared_examples_for_hookable_actions.rb
+++ spec/support/shared_examples_for_hookable_actions.rb
@@ -21,44 +21,4 @@ RSpec.shared_examples 'a hookable action' do
       expect(subject.send(:script_name)).to be_instance_of(String)
     end
   end
-
-  %w[before after].each do |point|
-    describe "#{point} hook" do
-      let(:perform) { subject.method(:perform) }
-      let(:cmd) { spy }
-      let(:bin_path) { File.join(subject.root.base_path, 'bin') }
-
-      before do
-        FileUtils.mkpath(bin_path)
-        allow(subject).to receive(:perform_without_script)
-      end
-
-      after { FileUtils.rm_rf(bin_path) }
-
-      context "when a #{point} hook exist?" do
-        before do
-          hook_name = "#{point}_#{script_name}"
-          hook_path = File.join(bin_path, hook_name)
-
-          FileUtils.touch(hook_path)
-          FileUtils.chmod(0o755, hook_path)
-
-          allow(TTY::Command).to receive(:new).and_return(cmd)
-          allow(cmd).to receive(:run)
-        end
-
-        it 'calls #perform_with_script' do
-          subject.perform
-          expect(cmd).to have_received(:run)
-        end
-      end
-
-      context 'when no before hook exist?' do
-        it 'calls #perform_without_script' do
-          subject.perform
-          expect(cmd).not_to have_received(:run)
-        end
-      end
-    end
-  end
 end
