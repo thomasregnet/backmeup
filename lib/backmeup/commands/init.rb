376e965a2e282b2a7d36e44cb6cbaf7a53d1b8c0
thomasregnet <ossi@asgard.localdomain>
2020-09-23 09:08:11 +0200

    Copy examples when initializing a repository

--- lib/backmeup/commands/init.rb
+++ lib/backmeup/commands/init.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+require 'tty-file'
 require_relative '../command'
 
 module Backmeup
   module Commands
-    # Initialize the directory structure
+    # Initialize the repository structure
     class Init < Backmeup::Command
+      REPOSITORY_DIRECTORIES = %w[backups bin config].freeze
+
       def initialize(path, options)
         @path = path
         @options = options
@@ -14,11 +17,12 @@ module Backmeup
       attr_reader :options, :path
 
       def execute(input: $stdin, output: $stdout)
-        # Command logic goes here ...
-        FileUtils.mkpath(path)
-        FileUtils.mkpath(File.join(path, 'backups'))
-        FileUtils.mkpath(File.join(path, 'bin'))
-        FileUtils.mkpath(File.join(path, 'config'))
+        REPOSITORY_DIRECTORIES.each do |dir|
+          FileUtils.mkpath(File.join(path, dir))
+        end
+
+        TTY::File.copy_directory('examples', File.join(path, 'examples'))
+        FileUtils.chmod(0o755, Dir.glob(File.join(path, 'examples/*')))
 
         output.puts "created #{path}"
       end
