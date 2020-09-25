6812071d9eeba6e643e4c179b800cfc4f7f35156
thomasregnet <ossi@asgard.localdomain>
2020-09-25 09:36:21 +0200

    Continue "initialize a repository and use it"

--- spec/integration/initialize_a_repository_and_use_it_spec.rb
+++ spec/integration/initialize_a_repository_and_use_it_spec.rb
@@ -1,17 +1,35 @@
 # frozen_string_literal: true
 
+require 'tty-file'
+
 RSpec.describe 'initialize a repository and use it' do
+  def backups
+    Dir.glob("#{File.join(repository_path, 'backups')}/*")
+  end
+
   def repository_path
     @repository_path ||= File.join('tmp', 'my_repository')
+  end
+
+  def data_path
+    @data_path ||= File.join('tmp', 'my_data')
   end
 
   # rubocop:disable RSpec/BeforeAfterAll
   before(:all) do
     `backmeup init #{repository_path}`
+
+    FileUtils.mkpath(data_path)
+    TTY::File.create_file(File.join(data_path, 'test'), "test\n")
+
+    filelist_path = File.join(repository_path, 'config', 'filelist')
+    TTY::File.create_file(filelist_path, data_path)
   end
 
-  after(:all) { FileUtils.rm_rf(repository_path) }
-  # rubocop:enable RSpec/BeforeAfterAll
+  after(:all) do
+    FileUtils.rm_rf(data_path)
+    FileUtils.rm_rf(repository_path)
+  end
 
   it 'creates the "backups" directory' do
     backups_dir = Pathname.new(File.join(repository_path, 'backups'))
@@ -32,4 +50,18 @@ RSpec.describe 'initialize a repository and use it' do
     examples_dir = Pathname.new(File.join(repository_path, 'examples'))
     expect(examples_dir).to be_directory
   end
+
+  describe '`backmeup create`' do
+    before(:all) { `backmeup create #{repository_path}` }
+
+    it 'created a backmeup' do
+      expect(backups.length).to eq(1)
+    end
+
+    it 'saved the data' do
+      test_path = File.join(backups.first, 'data', 'my_data', 'test')
+      expect(File.read(test_path)).to eq("test\n")
+    end
+  end
+  # rubocop:enable RSpec/BeforeAfterAll
 end
