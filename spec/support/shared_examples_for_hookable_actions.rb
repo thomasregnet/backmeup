7b7ef568bacf6dbb6b6226c81294bc7b484b58c5
thomasregnet <ossi@asgard.localdomain>
2020-12-10 19:37:54 +0100

    Comment shared_examples_for_hookable_actions

--- spec/support/shared_examples_for_hookable_actions.rb
+++ spec/support/shared_examples_for_hookable_actions.rb
@@ -4,18 +4,14 @@ RSpec.shared_examples 'a hookable action' do
   it { is_expected.to respond_to(:perform) }
   it { is_expected.to respond_to(:root) }
 
-  # describe '#cmd' do
-  #   it 'returns an instance of TTY::Command' do
-  #     expect(subject.send(:cmd)).to be_instance_of(TTY::Command)
-  #   end
-  # end
-
+  # the method #env may be protected so we use #send(:env)
   describe '#env' do
     it 'returns a hash' do
       expect(subject.send(:env)).to be_instance_of(Hash)
     end
   end
 
+  # the mehod #script_name may be protected so we use #send(:script_name)
   describe '#script_name' do
     it 'returns the script name as String' do
       expect(subject.send(:script_name)).to be_instance_of(String)
